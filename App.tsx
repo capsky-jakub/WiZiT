e79b520ab22d9f45c7d5ec0794a06edd3203137e
s1426 <s1426@SAB210421.sabris.int>
2025-12-21 10:33:44 +0100

    pokus na druhou
    Change-Id: I32117aee423ee9b27326037f9c9e6ddca1b7b06b
    Signed-off-by: s1426 <s1426@SAB210421.sabris.int>

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ import { setCacheExpirationDays } from './services/distanceCache';
 import { translations } from './services/translations';
 import { isClientScheduledForDate, sortVisitsByTime } from './services/scheduler';
 
-const DEFAULT_DEV_API_KEY = ""; 
+const DEFAULT_DEV_API_KEY = "test"; 
 
 const uuid = () => Math.random().toString(36).substring(2, 9);
 const commercialRound = (num: number) => Math.floor(num + 0.5);
